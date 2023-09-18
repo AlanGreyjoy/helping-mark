5952a3bf20896c8b79295ee94a369db6f33f49b5
Alan Spurlock <alanspurlock@hotmail.com>
2023-09-18 18:58:18 -0500

    init

---
+++ src/commands/race.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('race')
+    .setDescription('Add or remove a race time for a course'),
+  async execute(interaction) {
+    await interaction.reply('Hello world!');
+  },
+};
