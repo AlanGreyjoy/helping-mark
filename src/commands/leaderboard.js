7c37c79209672799570cbe3d4c95f6fdcbd37c8b
Alan Spurlock <alanspurlock@hotmail.com>
2023-09-18 22:44:19 -0500

    tonight

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,5 +1,10 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder, EmbedBuilder, ChannelType } = require('discord.js')
+
 const generators = require('../utils/generators')
+
+//Globals
+const courses = new Set()
+const leaderboardMessages = new Map()
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,15 +26,134 @@ module.exports = {
         }
       )
     )
-    .addStringOption((option) => option.setName('course').setDescription('Course name').setRequired(true)),
+    .addStringOption((option) => option.setName('course').setDescription('Course name').setRequired(false)),
 
   async execute(interaction) {
     const command = interaction.options.getString('command')
     const course = interaction.options.getString('course')
 
-    const courses = []
-    const leaderboardMessages = []
+    switch (command) {
+      case 'add':
+        if (!course) {
+          await interaction.reply({
+            content: 'Incomplete /leaderboard add command. Usage: /leaderboard add <course>',
+            ephemeral: true,
+          })
+          return
+        }
 
-    await interaction.reply('[INFO] This command is not yet implemented.')
+        addCourse(course, interaction)
+        break
+
+      case 'remove':
+        removeCourse(course, interaction)
+        break
+
+      case 'show':
+        showCourse(course, interaction)
+        break
+    }
   },
+}
+
+/**
+ * Add a course to the leaderboard
+ * @param {*} courseName
+ * @returns
+ */
+async function addCourse(courseName, interaction) {
+  console.log(`[INFO] Adding course ${courseName}...`)
+
+  const course = courseName.toLowerCase()
+
+  if (courses.has(course)) {
+    await interaction.reply({
+      content: 'Course already exists.',
+      ephemeral: true,
+    })
+
+    return
+  }
+
+  courses.add(course)
+
+  // Create a placeholder message in the same channel
+  const leaderboardMessage = await interaction.channel.send({
+    embeds: [await createLeaderboardEmbed(course)],
+  })
+
+  leaderboardMessages.set(course, leaderboardMessage)
+
+  await interaction.reply(`Course ${course} added.`)
+
+  // Store the message ID for future reference
+  leaderboardMessages.set(course, leaderboardMessage.id)
+
+  // Create a sub-thread for the course
+  const category = interaction.guild.channels.cache.find(
+    (channel) => channel.type === 'GUILD_CATEGORY' && channel.name === 'Leaderboards'
+  )
+
+  console.log(`[INFO] Found category ${category}`)
+
+  // Adjust the category name
+  const threadName = courseName
+  const autoArchiveDuration = 1440 // Adjust as needed
+
+  const thread = await interaction.channel.threads.create({
+    name: threadName,
+    autoArchiveDuration: autoArchiveDuration,
+  })
+
+  await thread.send(`Welcome to the leaderboard for ${courseName}!`)
+
+  return Promise.resolve()
+}
+
+/**
+ * Remove a course from the leaderboard
+ * @param {*} courseName
+ */
+async function removeCourse(courseName, interaction) {
+  console.log(`[INFO] Removing course ${courseName}...`)
+}
+
+/**
+ * Show a course from the leaderboard
+ * @param {*} courseName
+ */
+async function showCourse(courseName, interaction) {
+  console.log(`[INFO] Showing course ${courseName}...`)
+}
+
+/**
+ * Create a leaderboard embed
+ * @param {*} courseName
+ * @returns
+ */
+async function createLeaderboardEmbed(courseName, interaction) {
+  const leaderboardEmbed = new EmbedBuilder()
+    .setTitle(`Leaderboard for ${courseName}`)
+    .setDescription('Updated leaderboard goes here.')
+
+  return leaderboardEmbed
+}
+
+/**
+ * Edit the leaderboard message
+ * @param {*} interaction
+ * @param {*} courseName
+ */
+async function editLeaderboardMessage(interaction, courseName) {
+  // Edit the embedded message in the thread to include the updated leaderboard
+  const thread = interaction.guild.threads.cache.find((thread) => thread.name === courseName)
+
+  if (thread) {
+    const leaderboardEmbed = createLeaderboardEmbed(courseName) // Generate the updated leaderboard
+    const firstMessage = await thread.messages.fetchPinned().first()
+
+    if (firstMessage) {
+      await firstMessage.edit({ embeds: [leaderboardEmbed] })
+    }
+  }
 }
